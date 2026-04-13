93a0942b76ac6cb1396ae6f8526f12ee6463afdc
Matheus Gustav <265953245+MatheusGustav@users.noreply.github.com>
2026-04-13 14:17:04 -0300

    excluindo imagens antigas e add novas + pequenos ajustes.

---
+++ script.js
@@ -0,0 +1,32 @@
+function showSection(id, btnElement) {
+    document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+    document.querySelectorAll(".nav-bar button").forEach(btn => btn.classList.remove("active-btn"));
+    
+    document.getElementById(id).classList.add("active");
+    if(btnElement) {
+        btnElement.classList.add("active-btn");
+    }
+    
+    window.scrollTo({
+        top: document.querySelector('header').offsetHeight,
+        behavior: 'smooth'
+    });
+}
+
+function openCategory(evt, categoryName) {
+    // Esconde todas as categorias
+    var contents = document.getElementsByClassName("category-content");
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].classList.remove("active-category");
+    }
+
+    // Remove a classe 'active-tab' de todos os botões
+    var tabs = document.getElementsByClassName("tab-btn");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("active-tab");
+    }
+
+    // Mostra a categoria atual e adiciona a classe 'active-tab' ao botão clicado
+    document.getElementById(categoryName).classList.add("active-category");
+    evt.currentTarget.classList.add("active-tab");
+}
